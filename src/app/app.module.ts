8acbfc3c0707394059c47e81d57bd2b6fc6b15d3
DaoThao1205 <daothithao123voc@gmail.com>
2020-03-17 23:43:16 +0700

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -8,7 +8,7 @@ import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { SlideComponent } from './slide/slide.component';
 import { CategoryComponent } from './category/category.component';
-
+import { FormsModule} from '@angular/forms';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, ProductComponent, ProductListComponent, SlideComponent, CategoryComponent ],
