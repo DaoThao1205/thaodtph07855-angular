14acf889a27e09676ec65392b559effa1d9d08b3
DaoThao1205 <daothithao123voc@gmail.com>
2020-03-19 21:12:58 +0700

    5 files modified, 6 added

---
+++ src/app/MockData.ts
@@ -0,0 +1,36 @@
+
+import { Product } from './Product';
+export const data : Product[] = [
+    {
+      id: 1,
+      name: "Cherry",
+      price: 24.99,
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      status: false,
+      img: 'https://smartfruits.vn/wp-content/uploads/2019/03/cherry-duoc-trong-va-thu-hoach-tren-dat-my.jpg'
+    },
+    {
+      id: 2,
+      name: "Dâu Tây",
+      price: 24.99,
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      status: false,
+      img: 'https://files.vforum.vn/2014/T10/img/vforum.vn-39827-strawberry-wallpaper-yeah1-lc3.jpg'
+    },
+    {
+      id: 3,
+      name: "Sầu Riêng",
+      price: 24.99,
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      status: false,
+      img: 'https://ucamart.com/wp-content/uploads/2018/10/durian_fhwl.jpg'
+    },
+    {
+      id: 4,
+      name: "Kiwi",
+      price: 24.99,
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      status: false,
+      img: 'https://kenhvaobep.com/wp-content/uploads/2018/12/2168a1f3e24c.jpg'
+    }
+]
